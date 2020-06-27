875578a1c58edd2aca252f7de3ebcdef00f39393
Nexxer <yairopb@yandex.ru>
2020-06-27 19:15:54 +0300

    finish_css

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,19 @@
 const popup = document.querySelector('.popup')
 const popupButtonAdd = document.querySelector('.profile__button-edit');
 const popupButtonClose = document.querySelector('.popup__button-close');
+const popupPhotoView = document.querySelector('.element__image');
+const popupImgView = document.querySelector('.popup_type_photo');
+const popupButtonCloseImg = document.querySelector('.popup__button-close_for_img');
+const ButtonAddPlace = document.querySelector('.profile__button-add');
+const popupButtonAddPlace = document.querySelector('.popup_type_add-place')
+const popupButtonClosePlace = document.querySelector('.popup__button-close_for_place');
 
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__profession');
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let formElement = document.querySelector('#form-profile')
+
+const nameInput = document.querySelector('.popup__name');
+const jobInput = document.querySelector('.popup__profession');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const formElement = document.querySelector('#form-profile');
 
 function PopupToggle() {
   // Открытие и закрытие popup'a классом
@@ -18,11 +25,9 @@ function PopupToggle() {
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   // Получите значение полей из свойства value
-  let nameForProfile = nameInput.value;
-  let jobForProfile = jobInput.value;
-
+  const nameForProfile = nameInput.value;
+  const jobForProfile = jobInput.value;
   // Вставьте новые значения с помощью textContent
   profileName.textContent = nameForProfile;
   profileProfession.textContent = jobForProfile;
@@ -34,6 +39,23 @@ function formSubmitHandler(evt) {
 popupButtonAdd.addEventListener('click', PopupToggle);
 // Закрываем popup редактирование профиля крестиком
 popupButtonClose.addEventListener('click', PopupToggle);
-
 // Прикрепляем обработчик к форме: он будет следить за событием “submit”
 formElement.addEventListener('submit', formSubmitHandler);
+
+//Открываем просмотр фото по клику на превью
+popupPhotoView.addEventListener('click', function () {
+  popupImgView.classList.toggle('popup_opened');
+});
+// Закрываем попап с фото по крестику
+popupButtonCloseImg.addEventListener('click', function () {
+  popupImgView.classList.toggle('popup_opened');
+});
+
+//Открываем добавление места по кнопке
+ButtonAddPlace.addEventListener('click', function () {
+  popupButtonAddPlace.classList.toggle('popup_opened');
+});
+// Закрываем добавление места по крестику
+popupButtonClosePlace.addEventListener('click', function () {
+  popupButtonAddPlace.classList.remove('popup_opened');
+});

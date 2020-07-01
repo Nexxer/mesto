d41e866fff5a7f37aea47227c1e8992cf23b6d10
Nexxer <yairopb@yandex.ru>
2020-07-01 12:31:07 +0300

    1 init

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,140 @@
-const popup = document.querySelector('.popup')
-const popupButtonAdd = document.querySelector('.profile__button-edit');
-const popupButtonClose = document.querySelector('.popup__button-close');
-const popupPhotoView = document.querySelector('.element__image');
-const popupImgView = document.querySelector('.popup_type_photo');
-const popupButtonCloseImg = document.querySelector('.popup__button-close_for_img');
-const ButtonAddPlace = document.querySelector('.profile__button-add');
-const popupButtonAddPlace = document.querySelector('.popup_type_add-place')
-const popupButtonClosePlace = document.querySelector('.popup__button-close_for_place');
-const nameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__profession');
+// Профиль
+const popupEditProfile = document.querySelector('.profile__button-edit');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const formElement = document.querySelector('#form-profile');
-const placeElement = document.querySelector('.elememt__like')
+const popupProfile = document.querySelector('.popup_type_profile');
+const formPopupProfile = popupProfile.querySelector('#form-profile');
+const nameInputProfile = formPopupProfile.querySelector('.popup__name');
+const jobInputProfile = formPopupProfile.querySelector('.popup__compl');
 
-function PopupToggle() {
-  // Открытие и закрытие popup'a классом
-  popup.classList.toggle('popup_opened');
-  // Задаем значения в popup'e которые есть в профайле на данный момент
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileProfession.textContent;
+// Добавление места
+const popupAddPlace = document.querySelector('.profile__button-add');
+const placeList = document.querySelector('.elements__list');
+const placeTemplate = placeList.querySelector('#element');
+const popupPlace = document.querySelector('.popup_type_add-place');
+const formpopupPlace = popupPlace.querySelector('#form-place');
+const placeInput = formpopupPlace.querySelector('#place-name');
+const linkInput = formpopupPlace.querySelector('#place-link');
+
+
+// Просмотр фото
+const popupImage = document.querySelector('.popup_type_photo');
+const placeImage = popupImage.querySelector('.popup__image');
+const placeTitle = popupImage.querySelector('.popup__caption');
+
+// Открытие попапов
+const PopupToggle = function (evt) {
+  evt.classList.toggle('popup_opened');
+};
+
+// Закрытия попапов
+const closeButtonPopupProfile = popupProfile.querySelector('.popup__button-close');
+const closeButtonpopupImage = popupImage.querySelector('.popup__button-close');
+const closeButtonpopupPlace = popupPlace.querySelector('.popup__button-close');
+
+const closePopupProfile = function (evt) {
+  if (evt.target != evt.currentTarget) {
+    return
+  }
+  PopupToggle(popupProfile);
 }
+const closepopupImage = function (evt) {
+  if (evt.target != evt.currentTarget) {
+    return
+  }
+  PopupToggle(popupImage);
+}
+const closepopupPlace = function (evt) {
+  if (evt.target != evt.currentTarget) {
+    return
+  }
+  PopupToggle(popupPlace);
+};
 
-function formSubmitHandler(evt) {
+// Заполнение форм профиля
+const formSubmitHandlerProfile = function (evt) {
+  event.preventDefault();
+  profileName.textContent = nameInputProfile.value;
+  profileProfession.textContent = jobInputProfile.value;
+  PopupToggle(popupProfile);
+};
+
+// Заполняем ввод в попапе профиля текущими значениями профиля
+const openPopup = function () {
+  PopupToggle(popupProfile);
+  nameInputProfile.value = profileName.textContent;
+  jobInputProfile.value = profileProfession.textContent;
+};
+
+// Массив мест
+const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Функция добавления места
+function addPlace(name, link) {
+  const place = placeTemplate.content.cloneNode(true);
+  place.querySelector('.element__title').textContent = name;
+  place.querySelector('.element__image').src = link;
+  place.querySelector('.element__image').alt = name;
+  place.querySelector('.element__delete').addEventListener('click', (evt) => evt.target.closest('.element').remove());
+  place.querySelector('.elememt__like').addEventListener('click', (evt) => evt.target.classList.toggle('elememt__like_active'));
+  place.querySelector('.element__image').addEventListener('click', function (evt) {
+    PopupToggle(popupImage);
+    placeImage.setAttribute('src', evt.target.src);
+    placeTitle.textContent = name;
+    placeImage.setAttribute('alt', name);
+  });
+  placeList.prepend(place);
+  return place;
+};
+
+// Изначальные места
+initialCards.forEach(element => addPlace(element.name, element.link));
+
+// Добавляем место
+function formSubmitHandlerPlace(evt) {
   evt.preventDefault();
-  // Получите значение полей из свойства value
-  const nameForProfile = nameInput.value;
-  const jobForProfile = jobInput.value;
-  // Вставьте новые значения с помощью textContent
-  profileName.textContent = nameForProfile;
-  profileProfession.textContent = jobForProfile;
-  // Закрываем popup
-  PopupToggle();
-}
+  addPlace(placeInput.value, linkInput.value);
+  PopupToggle(popupPlace);
+};
 
-// Открываем popup редактирование профиля по кнопке редактирования
-popupButtonAdd.addEventListener('click', PopupToggle);
-// Закрываем popup редактирование профиля крестиком
-popupButtonClose.addEventListener('click', PopupToggle);
-// Прикрепляем обработчик к форме: он будет следить за событием “submit”
-formElement.addEventListener('submit', formSubmitHandler);
+// Слушаем события
+closeButtonPopupProfile.addEventListener('click', () => PopupToggle(popupProfile));
+popupEditProfile.addEventListener('click', openPopup);
+formPopupProfile.addEventListener('submit', formSubmitHandlerProfile);
+popupImage.addEventListener('click', closepopupImage);
+popupProfile.addEventListener('click', closePopupProfile);
+popupPlace.addEventListener('click', closepopupPlace);
+formpopupPlace.addEventListener('submit', formSubmitHandlerPlace);
+closeButtonpopupImage.addEventListener('click', () => PopupToggle(popupImage));
+closeButtonpopupPlace.addEventListener('click', () => PopupToggle(popupPlace));
 
-//Открываем просмотр фото по клику на превью
-popupPhotoView.addEventListener('click', function () {
-  popupImgView.classList.toggle('popup_opened');
-});
-// Закрываем попап с фото по крестику
-popupButtonCloseImg.addEventListener('click', function () {
-  popupImgView.classList.toggle('popup_opened');
-});
-
-//Открываем добавление места по кнопке
-ButtonAddPlace.addEventListener('click', function () {
-  popupButtonAddPlace.classList.toggle('popup_opened');
-});
-// Закрываем добавление места по крестику
-popupButtonClosePlace.addEventListener('click', function () {
-  popupButtonAddPlace.classList.toggle('popup_opened');
-});
-
-// Меняем иконку лайка при клике
-placeElement.addEventListener('click', function () {
-  console.log('Da');
-  placeElement.classList.toggle('elememt__like_active');
+// Очищаем поля ввода места
+popupAddPlace.addEventListener('click', () => {
+  PopupToggle(popupPlace);
+  placeInput.value = '';
+  linkInput.value = '';
 });
